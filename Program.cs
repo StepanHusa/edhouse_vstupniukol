19e20425380d061e4489258c367e6e296ca5f6bc
StepanHusa <stepan.husa@gmail.com>
2022-05-24 13:44:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vstupniukol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dobrý den,");
+            Console.WriteLine("Jmenuji se Štěpán Husa a tento program jsem napsal jako vstupní úkol pro stáž ve firmě Edhouse");
+            Console.WriteLine("Nyní stiskněte Enter pro načtení .txt souboru pomocí windows dialogu");
+            Console.ReadKey();
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        }
+    }
+}
